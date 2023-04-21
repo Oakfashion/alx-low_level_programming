36a0514f9f88b4dba7648f75c3233476c9caf311
Oakfashion <Lawalkudirat2019@gmail.com>
2023-04-21 10:46:51 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - 5-printf.c
  * description - Write a C program that prints exactly with proper grammar,
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
