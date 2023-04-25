49fbbebda95dfe65d71ee5eea1a890aebd10f985
Oakfashion <Lawalkudirat2019@gmail.com>
2023-04-24 21:08:52 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	chat j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putcahr(j);
+	putchat('\n');
+	return (0);
+}
