d2dc61454301db23daf021d8eed182c76de58b15
Oakfashion <Lawalkudirat2019@gmail.com>
2023-04-28 14:26:39 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,15 +5,15 @@
  * print_line - Draws a straight line in the terminal
  * @n: The number of times the character _ should be printed
  */
-int i;
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		putchar('\n');
 		return;
 	}
-	
 	i = 0;
 	for (i = 0; i < n; i++)
 	{
