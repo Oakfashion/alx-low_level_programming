d687a71c78b9b2d81fa7134ee3f8de6dfe614fb9
Oakfashion <Lawalkudirat2019@gmail.com>
2023-05-02 14:52:06 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,30 @@
 #include "main.h"
 /**
- * put2 - print strings
- * @str: function parameter
- * Return: 0
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
-
-void rev_string(char *s)
+void puts2(char *str)
 {
-        int i;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-        for (i = 0 ; str[i] != '\0' ; i++)
-        {
-                _putchar(str[i]);
-                i++;
-        }
-        _putchar('\n');
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
 }
+
