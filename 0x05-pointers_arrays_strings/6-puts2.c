84548ddba95e53272d1a868cd79e9aa8cf42024e
Oakfashion <Lawalkudirat2019@gmail.com>
2023-05-02 04:19:05 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * put2 - print strings
+ * @str: function parameter
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+        int i;
+
+        for (i = 0 ; str[i] != '\0' ; i++)
+        {
+                _putchar(str[i]);
+                i++;
+        }
+        _putchar('\n');
+}
