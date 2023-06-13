ddadded91ed6862f58bb6d77ec1e718216eb0f6d
Oakfashion <Lawalkudirat2019@gmail.com>
2023-06-13 10:53:55 -0700

    *update

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - Prints an integer using only _putchar
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	putchar((num % 10) + '0');
+}
